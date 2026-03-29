179355fbaf10f363ff0cd1f6b4b145c43846d5d4
Snuffy2 <6526076+Snuffy2@users.noreply.github.com>
2026-03-29 02:06:33 +0000

    Updating to version v1.0.4 [skip ci]

--- aiopnsense/const.py
+++ aiopnsense/const.py
@@ -4,7 +4,7 @@ from typing import Any
 
 from dateutil.tz import gettz
 
-VERSION = "v1.0.3"
+VERSION = "v1.0.4"
 
 # Default timeout, in seconds, for API requests.
 DEFAULT_REQUEST_TIMEOUT_SECONDS = 60
