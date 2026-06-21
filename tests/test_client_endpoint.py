7e6b9535a24760ea3f0c63c2397948547663808a
Snuffy2 <Snuffy2@users.noreply.github.com>
2026-06-20 20:46:12 -0400

    Update test_client_endpoint.py

--- tests/test_client_endpoint.py
+++ tests/test_client_endpoint.py
@@ -810,7 +810,7 @@ async def test_get_endpoint_path_lazily_initializes_snake_case_state(
             client._use_snake_case = False
 
         set_use_snake_case = AsyncMock(side_effect=fake_set_use_snake_case)
-        setattr(client, "_set_use_snake_case", set_use_snake_case)
+        client._set_use_snake_case = set_use_snake_case
 
         assert await client._get_endpoint_path("/snake_case", "/camelCase") == "/camelCase"
         set_use_snake_case.assert_awaited_once_with()
